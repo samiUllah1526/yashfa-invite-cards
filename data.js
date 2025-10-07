4a4795512ece4159aa60abee05b198a6072e8b5f
sami-ullah <sami.naazar1526@gmail.com>
2025-10-08 00:18:10 +0500

    add cards links

---
+++ data.js
@@ -0,0 +1,271 @@
+const NIKAH_CARDS = [
+  {
+    id: 1,
+    name: "Royal Garden - Digital Nikkah Invitation",
+   price: "450/.",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758678303/no.1royal_garden_t4qkg4.jpg",
+  },
+  {
+    id: 2,
+    name: "Nature's Grace - Digital Nikkah Invitation",
+    price: "450/.",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758678298/no.2naturegrace_ybuyfp.jpg",
+  },
+  {
+    id: 3,
+    name: "Pastel Grace-Digital Nikkah invitation",
+    price: "450/.",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758678300/no.3pastelgrace_ykip4p.jpg",
+  },
+  {
+    id: 4,
+    name: "Olive Charm - Digital Nikkah Invitation",
+   price: "450/.",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758678299/no.4pastelgrace_ki8hk8.jpg",
+  },
+  {
+    id: 5,
+    name: "Golden Grace - Digital Nikkah invitation",
+   price: "350/.",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758678305/no.5goldengrace_nosoln.jpg"
+  },
+  {
+    id: 6,
+    name: "Rosegate Elegance - Digital nikkah Card",
+   price: "350/.",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758678305/no.6rosegate_xalluu.jpg",
+  },
+  {
+    id: 7,
+    name: "Divine Bond Digital Nikkah Card",
+    price: "350/.",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758678305/no.8mysticgreen_xvqdvb.jpg"
+  },
+  {
+    id: 8,
+    name: "Mystic Green - Digital nikkah invitation",
+    price: "350/.",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758678303/no.7divinebond_tnnmk0.jpg",
+      }
+ 
+]
+
+export const ENGAGMENT_CARDS = [
+  {
+    id: 1,
+    name: "Blooming Love - Digital Engagement Card.",
+    price: "450/.",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758678782/no.1bloominglove_kzp5qp.jpg",
+  },
+  {
+    id: 2,
+    name: "Golden Bond - Digital Engagement Card.",
+    price: "450/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758678783/no.2golden_moddas.jpg",
+  },
+  {
+    id: 3,
+    name: "Forever Promise - Digital Eng Card.",
+     price: "450/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758678784/no.3forever_ttdpws.jpg",
+  },
+  {
+    id: 4,
+    name: "Botanical Bliss - Digital Engagement Card.",
+     price: "450/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758678779/no.4Botanical_Bliss_ws94hd.jpg",
+  }
+]
+
+
+export const MEHNDI_CARDS = [
+  {
+    id: 1,
+    name: "Golden Charm - Digital Mehndi Card",
+   price: "450/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758680439/no.1_wrhokx.jpg",
+  },
+  {
+    id: 2,
+    name: "Green Dream - Digital Mehndi Card",
+    price: "450/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758680446/no.2_jbwqc6.jpg",
+  },
+    {
+    id: 3,
+    name: "Festive Vibes - Digital Mehndi Card",
+    price: "450/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758680444/no.3_gzcsif.jpg",
+  },
+  {
+    id: 4,
+    name: "Sparkle Love - Digital Mehndi Card",
+    price: "450/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758680443/no.4_ctci2t.jpg",
+  },
+   {
+    id: 5,
+    name: "Royal Curtain - Digital Mehnadi Card",
+    price: "450/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758680446/no.5_utdi0b.jpg",
+  },
+   {
+    id: 6,
+    name: "Outdoor Garden - Digital Mehndi Card",
+    price: "450/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758680448/no.6_q4t33v.jpg",
+  },
+  {
+    id: 7,
+    name: "Classic Green - Digital Mehndi Card",
+    price: "450/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758680449/no.7_jh5mlf.jpg",
+  },
+   {
+    id: 8,
+    name: "Golden Drape - Digital Mehndi Card",
+    price: "450/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758680453/no.8_amaeme.jpg",
+  }
+]
+
+export const BARAT_CARDS = [
+  {
+    id: 1,
+    name: "Regal Velvet - Digital Barat Invitation.",
+    price: "450/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758681390/no.1_qj3cjc.jpg",
+  },
+  {
+    id: 2,
+    name: "Emerald Valley - Digital Barat Invitation",
+    price: "450/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758681390/no.2_aixasq.jpg",
+  },
+   {
+    id: 3,
+    name: "Ivory Blossom - Digital Barat Invitation",
+    price: "450/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758681395/no.3_wlhfth.jpg",
+  },
+   {
+    id: 4,
+    name: "Crimson Elegance - Digital Barat Invitation.",
+    price: "450/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758681399/no.4_om3nzm.jpg",
+  },
+   {
+    id: 5,
+    name: "Royal Midnight - Digital Barat Invitation",
+    price: "450/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758681398/no.5_axgw37.jpg",
+  },
+  {
+    id: 6,
+    name: "Majestic Bloom - Digital Barat Invitation.",
+    price: "450/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758681401/no.6_wuviqn.jpg",
+  },
+   {
+    id: 7,
+    name: "Imperial Grace - Digital Barat Invitation.",
+    price: "450/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758681396/no.7_s9lhve.jpg",
+  }
+  
+]
+
+export const WALIMA_CARDS = [
+  {
+    id: 1,
+    name: "Luxe Bond - Digital Walima Invitation",
+    price: "450/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758681979/no.1_mkrxej.jpg",
+  },
+  {
+    id: 2,
+    name: "Golden Grace - Digital Walima Invitation",
+    price: "450/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758681983/no.2_wggb9e.jpg",
+  },
+  {
+    id: 3,
+    name: "Crystal Bloom - Digital Walima Card",
+    price: "450/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758681982/no.3_xvicf5.jpg",
+  },
+  {
+    id: 4,
+    name: "EEternal Blossom - Digital Walima Card",
+    price: "450/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758681984/no.4_ortc5s.jpg",
+  },
+  {
+    id: 5,
+    name: "Engagement Card 1Royal Serenity - Walima Card",
+    price: "450/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758681984/no.5_xjvhwp.jpg",
+  }
+  
+]
+
+export const BIRTHDAY_CARDS = [
+  {
+    id: 1,
+    name: "Princess Bloom - Digital Birthday Invitation.",
+    price: "650/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758683772/no.1_lr3tva.jpg",
+  },
+  {
+    id: 2,
+    name: "Sky Explorer - Digital Birthday Invitation.",
+    price: "650/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758683774/no.2_h6tqvo.jpg",
+  },
+   {
+    id: 3,
+    name: "Fairyland Tales - Digital Birthday Invitation.",
+    price: "650/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758683777/no.3_xqkf9i.jpg",
+  },
+   {
+    id: 4,
+    name: "Tiny Tracks - Digital Birthday Invitation.",
+    price: "650/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758683774/no.4_rv4hmc.jpg",
+  },
+   {
+    id: 5,
+    name: "Pink Castle - Digital Birthday Card.",
+    price: "650/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758683775/no.5_ikyzrl.jpg",
+  },
+   {
+    id: 6,
+    name: "Aqua Dreams - Digital Birthday Card.",
+    price: "500/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758683780/no.6_ajmyyk.jpg",
+  },
+   {
+    id: 7,
+    name: "Honey Bear Express - Digital Birthday Card.",
+    price: "500/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758683780/no.7_qvnscq.jpg",
+  },
+     {
+    id: 8,
+    name: " Golden Hive - Digital Birthday Card.",
+    price: "500/-",
+    url: "https://res.cloudinary.com/dzx8o8drh/image/upload/v1758683780/no.8_uhfknt.jpg",
+  },
+]
+
+
+export const cardsData = {
+  NIKAH_CARDS,
+  ENGAGMENT_CARDS,
+  MEHNDI_CARDS,
+  BARAT_CARDS,
+  WALIMA_CARDS,
+  BIRTHDAY_CARDS,
+}
